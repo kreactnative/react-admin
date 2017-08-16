b181270120b79b882532e8705bd29a038a2b33ee
Levon Kirakosyan <noosx.e@gmail.com>
2017-08-16 00:10:29 -0700

    Add section wrapper grid, and section title

--- src/pages/section/Section.jsx
+++ src/pages/section/Section.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+
 const Section = () => (
-  <section></section>
+  <section style={{ paddingTop: '80px', marginLeft: '300px', marginRight: '50px' }} >
+    <Grid container spacing={24}>
+      <Grid item xs={12}>
+        <Paper style={{ padding: '16px' }}>
+          <Typography type="title">Section</Typography>
+        </Paper>
+      </Grid>
+    </Grid>
+  </section>
 );
 
 export default Section;
